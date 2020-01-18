c88711a7639384c642acc255d4c6c4ce64c57e5c
jujuju0924 <jumpei0924@gmail.com>
2020-01-18 15:36:23 +0900

    マイページページ遷移

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
 
-  root 'mypage#mypage'
 
 
   devise_for :users, controllers: {
@@ -22,7 +21,14 @@ Rails.application.routes.draw do
       get 'done'
     end
   end
-  
-  root 'mypage#index'
+  resources :mypage do
+    collection  do
+      get 'index'
+      get 'identification'
+      get 'credit_card'
+      get 'user_profile'
+      get 'logout'
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
