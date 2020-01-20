e6d492139aebc41f5a4ed634bad9e9daaf6c79e0
yuya <yuevo16@gmail.com>
2020-01-20 13:14:27 +0900

    プルリクします

--- app/controllers/signup_controller.rb
+++ app/controllers/signup_controller.rb
@@ -91,10 +91,15 @@ class SignupController < ApplicationController
       @card.save
         # ログインするための情報を保管
         session[:id] = @user.id
-        redirect_to '/signup/done'
+        redirect_to done_signup_index_path
     else
       render '/signup/step1'
     end
+
+    def done
+      sign_in User.find(session[:id]) unless user_signed_in?
+    end
+
   end
 
 
