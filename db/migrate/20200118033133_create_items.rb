80d498200dd3440a642fbb10d86da441eb0e8ad1
takaking216 <takaking216@yahoo.co.jp>
2020-01-24 19:21:26 +0900

    カラムタイプ修正、

--- db/migrate/20200118033133_create_items.rb
+++ db/migrate/20200118033133_create_items.rb
@@ -3,19 +3,19 @@ class CreateItems < ActiveRecord::Migration[5.0]
     create_table :items do |t|
       t.string :name, null: false
       t.text :info, null: false
-      t.string :category, null: false
-      t.string :category1
-      t.string :category2
+      t.integer :category, null: false
+      t.integer :category1
+      t.integer :category2
       t.string :brand
-      t.string :status, null: false
-      t.string :delivery_chage, null: false
-      t.string :delivery_area, null: false
-      t.string :delivery_method
-      t.string :delivery_date, null: false
+      t.integer :status, null: false
+      t.integer :delivery_chage, null: false
+      t.integer :delivery_area, null: false
+      t.integer :delivery_method
+      t.integer :delivery_date, null: false
       t.integer :price, null: false
-      t.string :deal
-      t.string :saler
-      t.string :buyer
+      t.integer :deal
+      t.integer :saler
+      t.integer :buyer
       t.references :user, foreign_key: true
       t.timestamps
     end
