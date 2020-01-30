43fad678010839a7bb6dd0ff7f046b93848b9c88
yuya <yuevo16@gmail.com>
2020-01-30 16:07:05 +0900

    検索機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get 'searches/index'
+
   devise_for :users, controllers: {
     omniauth_callbacks: 'users/omniauth_callbacks',
     registrations: 'users/registrations',
