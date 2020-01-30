8c08afd8a841dec6f70c131266243dd3d75f4617
nikaidowataru <nnn.www.0319@gmail.com>
2020-01-30 15:12:19 +0900

    Update application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,8 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require turbolinks
-//= require jquery
 
+//= require jquery
+//= require jquery_ujs
+//= require turbolinks
 //= require_tree .
